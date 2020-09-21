fbba98832f0f0a50d69c2a261ab3972308a77be3
Brock Byrd <brockbyrdd@gmail.com>
2020-09-21 18:47:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,18 +4,22 @@ class Author
 
   def initialize(name)
     @name = name
+    @posts = []
   end
 
   def posts
-
+    Post.all.select {|post| post.author == self}
   end
 
   def add_post(post)
-
+    @posts << post
+    post.author
   end
 
   def add_post_by_title(title)
-
+    post = Post.new(title)
+    add_post(post)
+    post.author
   end
 
   def self.post_count
