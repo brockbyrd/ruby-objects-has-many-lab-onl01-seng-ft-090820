acf6aa055d73ed49ccfb87b88db8970f95634411
Brock Byrd <brockbyrdd@gmail.com>
2020-09-21 18:30:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,4 +4,22 @@ class Artist
   def initialize(name)
     @name = name
   end
+
+  def songs
+
+  end
+
+  def add_song(song)
+
+  end
+
+  def add_song_by_name
+
+  end
+
+  def self.song_count
+
+  end
+
+
 end
