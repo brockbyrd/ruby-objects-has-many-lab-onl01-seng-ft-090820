da58ea021a8562bcaee37e12e1a5fe7e87e9efa9
Brock Byrd <brockbyrdd@gmail.com>
2020-09-21 19:15:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name
 
+  @@song_count = 0
+
   def initialize(name)
     @name = name
     @songs = []
@@ -13,12 +15,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
     song.artist
+    @@song_count += 1
   end
 
   def self.song_count
