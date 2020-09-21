780e1346a746c93a6154c3eda799a8fc447742d4
Brock Byrd <brockbyrdd@gmail.com>
2020-09-21 18:44:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ class Artist
   end
 
   def songs
-
+    Song.all.select {|song| song.artist == self}
   end
 
   def add_song(song)
@@ -17,11 +17,12 @@ class Artist
 
   def add_song_by_name(name)
     song = Song.new(name)
+    add_song(song)
     song.artist
   end
 
   def self.song_count
-    @@all.length
+
   end
 
 
