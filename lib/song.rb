98b4a212b66ff786a408518a1caeda47aaa5fe2f
Brock Byrd <brockbyrdd@gmail.com>
2020-09-21 18:35:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,19 @@
 class Song
 
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def artist_name
+
+  end
+
+  def self.all
+    @@all
+  end
 end
